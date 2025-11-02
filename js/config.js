c266d90f7a9360e2fff1df4f9beb3d119c97f738
Fatima Anab <fxtymaanab@gmail.com>
2025-11-02 19:03:09 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_sg22tKkJ4', 
-        userPoolClientId: '1tgahri39rav6nl20fj4mh4fmf', 
+        userPoolId: 'us-east-1_7IsxNYvgO', 
+        userPoolClientId: '16b8c1cjn8jnkpb6lb2adam3v0', 
         region: 'us-east-1'
     },
     api: {
         invokeUrl: 'https://hfmrzfkghl.execute-api.us-east-1.amazonaws.com/dev' 
     }
 };
+
 
 
 
